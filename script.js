f6782f8dd0393331080b2dcb174d65f28fdaa8d8
Elliot <obukoyibooghenefejiro@gmail.com>
2025-03-16 00:59:17 +0100

    get

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+
+//header styles
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnicon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+
+    toggleBtnicon.classList = isOpen
+    ?'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
+}
+
+
 document.getElementById('messageForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
